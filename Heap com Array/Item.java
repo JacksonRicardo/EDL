46dbffd991433dedd809d871235ec909bdf4dd00
Jackson Ricardo <42697964+JacksonRicardo@users.noreply.github.com>
2023-01-12 18:08:54 +0000

    Melhorando a identação

--- Heap com Array/Item.java
+++ Heap com Array/Item.java
@@ -1,20 +1,24 @@
 public class Item {
-private int chave;
-private Object elemento;
-public Item(int k, Object o) {
-setChave(k);
-setElemento(o);
+  private int chave;
+  private Object elemento;
+  public Item(int k, Object o) {
+    setChave(k);
+    setElemento(o);
 }
-public int getChave() {
-return chave;
+
+  public int getChave() {
+    return chave;
 }
-public void setChave(int chave) {
-this.chave = chave;
+  
+  public void setChave(int chave) {
+    this.chave = chave;
 }
-public Object getElemento() {
-return elemento;
+  
+  public Object getElemento() {
+    return elemento;
 }
-public void setElemento(Object elemento) {
-this.elemento = elemento;
+  
+  public void setElemento(Object elemento) {
+    this.elemento = elemento;
   }
 }
--- Heap com Array/Main.java
+++ Heap com Array/Main.java
@@ -1,58 +1,54 @@
 import java.util.Scanner;
 
 public class Main {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int op = 1;
-Object elemento;
-int chave;
-Heap h = new Heap();
-while (op != 0) {
-System.out.print("Comandos:\n[1] insert\n[2] removeMin\n[3]min\n[4] size\n[5] Mostrar Heap\n[0] Sair\n -> ");
-op = sc.nextInt();
-switch (op) {
-case 1:
-System.out.print("Chave de prioridade: ");
-chave = sc.nextInt();
-System.out.print("Elemento: ");
-elemento = sc.next();
-h.insert(chave, elemento);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int op = 1;
+    Object elemento;
+    int chave;
+    Heap h = new Heap();
+    while (op != 0) {
+      System.out.print("Comandos:\n[1] insert\n[2] removeMin\n[3]min\n[4] size\n[5] Mostrar Heap\n[0] Sair\n -> ");
+      op = sc.nextInt();
+      switch (op) {
+        case 1:
+          System.out.print("Chave de prioridade: ");
+          chave = sc.nextInt();
+          System.out.print("Elemento: ");
+          elemento = sc.next();
+          h.insert(chave, elemento);
+          break;
+          
+        case 2:
+          if (h.isEmpty()) {
+          System.out.println("Heap vazio.");
+          } else {
+          Item i = h.removeMin();
+          System.out.println("Removido o Item de Chave [" + i.getChave() + "] e Elemento [" + i.getElemento() + "]");
+          }
+          break;
 
-break;
-case 2:
-if (h.isEmpty()) {
-System.out.println("Heap vazio.");
-} else {
-Item i = h.removeMin();
-System.out.println("Removido o Item de Chave ["
-+ i.getChave() + "] e Elemento [" +
+        case 3:
+          if (h.isEmpty()) {
+          System.out.println("Heap vazio.");
+          } else {
+          System.out.println("O min é o Item de Chave [" + h.min().getChave() + "] e Elemento [" + h.min().getElemento() + "]");
+          }
+          break;
 
-i.getElemento() + "]");
-}
-break;
-case 3:
-if (h.isEmpty()) {
-System.out.println("Heap vazio.");
-} else {
-System.out.println("O min é o Item de Chave ["
-+ h.min().getChave() + "] e Elemento [" +
-
-h.min().getElemento() + "]");
-}
-break;
-case 4:
-System.out.println("Size = " + h.size());
-break;
-case 5:
-if (h.isEmpty()) {
-System.out.println("Heap vazio.");
-} else {
-h.showHeap();
-}
-default:
-break;
-}
-}
-sc.close();
+        case 4:
+          System.out.println("Size = " + h.size());
+          break;
+        case 5:
+          if (h.isEmpty()) {
+            System.out.println("Heap vazio.");
+          } else {
+            h.showHeap();
+          }
+        default:
+          break;
+      }
+    }
+    sc.close();
   }
 }
